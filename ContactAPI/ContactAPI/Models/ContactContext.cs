10d51b050945d9215840d6856ecbffa98e33c101
shray7 <shray.7@gmail.com>
2015-11-08 00:42:01 -0500

    Added Entity framework and ContactContext

---
+++ ContactAPI/ContactAPI/Models/ContactContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ContactAPI.Models
+{
+    public class ContactContext  : DbContext
+    {
+        public DbSet<Contact> ContactSet { get; set; }
+        public ContactContext() : base("DefaultConnection")
+        { }
+
+        public virtual void Delete(Contact contact)
+        {
+            ContactSet.Attach(contact);
+            ContactSet.Remove(contact);
+            SaveChanges();
+        }
+    }
+}
